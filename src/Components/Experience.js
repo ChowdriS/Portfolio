f49120bd78785b879191daa6ff55e35436ade913
ChowdriS <114232036+ChowdriS@users.noreply.github.com>
2023-09-22 00:09:49 +0530

    experience

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -2,25 +2,36 @@ import React, { use, useRef } from 'react'
 import { motion,useScroll } from 'framer-motion'
 import LiIcon from './LiIcon'
 
-const Details = ({position,company,companyLink,time,address,work}) =>{
-    const ref = useRef(null)
-    return <li ref={ref} className='my-8 first:mt-0 last:mb:0 w-[60%] mx-auto flex items-center justify-between'>
-        <LiIcon reference="ref"/>
-        <motion.div 
-        initial={{y:50}}
-        whileInView={{y:0}}
-        transition={{duration:0.5,type:"spring"}}
+const Details = ({ position, company, companyLink, time, address, work }) => {
+    const ref = useRef(null);
+    return (
+        <li
+            ref={ref}
+            className='my-8 first:mt-0 last:mb:0 w-[60%] mx-auto flex items-center justify-between'
         >
-            <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a 
-            href={companyLink} className='text-sky-700 capitalize'
-            >-{company}</a></h3>
-            <span className='capitalize font-medium text-dark/75'>
-                {time} | {address}
-            </span>
-            <p className='font-medium w-full'>{work}</p>
-        </motion.div>
-    </li>
-}
+            <LiIcon reference="ref" />
+            <motion.div
+                initial={{ y: 50 }}
+                whileInView={{ y: 0 }}
+                transition={{ duration: 0.5, type: "spring" }}
+            >
+                <h3 className='capitalize font-bold text-2xl' style={{ whiteSpace: 'nowrap' }}>
+                    {position}&nbsp;<a
+                        href={companyLink}
+                        className='text-sky-700 capitalize'
+                    >
+                        -{company}
+                    </a>
+                </h3>
+                <span className='capitalize font-medium text-dark/75'>
+                    {time} | {address}
+                </span>
+                <p className='font-medium w-full'>{work}</p>
+            </motion.div>
+        </li>
+    );
+};
+
 
 const Experience = () => {
     const ref=useRef(null)
@@ -29,11 +40,11 @@ const Experience = () => {
         offset:['start end',"center start"]
     })
   return (
-    <div className='mt-32 flex items-center justify-center'>
-        <h2 className='font-bold text-8xl mb-32 text-center'>
+    <div className=' w-full mt-32 flex items-center justify-center'>
+        <h2 className='font-bold text-8xl mx-8 mb-32 text-center'>
         My Journey
         </h2>
-        <div ref={ref} className='w-[75%] mx-auto relative'>
+        <div ref={ref} className='w-full mx-auto relative'>
 
             <motion.div  style={{scaleY:scrollYProgress}}
             className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'></motion.div>
