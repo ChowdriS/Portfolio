11d3b12d91573ba44e7996ef72af17b32a818f1a
ChowdriS <114232036+ChowdriS@users.noreply.github.com>
2023-09-27 19:53:08 +0530

    made my day

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
         <Layout className='py-8 flex items-center justify-between'>
             <span>{new Date().getFullYear()}&copy;All Rights Reserved!</span>
-            <div className='flex items-center'>Build with <span className='text-rose-600 text-3xl px-1'>&hearts;</span>
+            <div className='flex items-center'>Crafted with <span className='text-rose-600 text-3xl px-1'>&hearts;</span>
                 <span>by&nbsp;</span>
             <h2 className='text-sky-900'>Chow3</h2>
             </div>
